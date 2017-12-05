a14afbfb5cb3fd38cf3bb867d89ac705c973c498
Zacharias Hellberg <zacharias.hellberg@student.oulu.fi>
2017-12-05 22:00:11 +0200

    Solved the second part of cal4 and added a function to test for the anagrams.

--- calendar4.py
+++ calendar4.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def duplicateChecker(passwordList):
     duplicate = False
     for word1 in range(len(passwordList)):
@@ -11,12 +10,40 @@ def duplicateChecker(passwordList):
                 pass
     return duplicate
 
+def anagramChecker(passwordList):
+    anagram = False
+    for word1 in passwordList:
+        for word2 in passwordList:
+            wordLength=len(word1)
+            wordRisingLength=0
+            if len(word1) != len(word2):
+                pass
+            else:
+                for i in word2:
+                    for j in word1:
+                        if i == j:
+                            wordRisingLength+=1
+                            break
+                        else:
+                            pass
+                if word1!=word2 and wordRisingLength==wordLength:
+                    anagram = True
+                    break
+        if anagram == True:
+            break
+        else:
+            pass
+    return anagram
+
+
+
 validSum = 0
 with open(sys.argv[1]) as f:
     for line in f:
         passList = line.split()
         duplicate = duplicateChecker(passList)
-        if duplicate == False:
+        anagram = anagramChecker(passList)
+        if duplicate == False and anagram == False:
             validSum += 1
         else:
             pass
